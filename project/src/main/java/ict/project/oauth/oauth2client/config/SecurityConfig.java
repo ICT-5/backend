a339539e7e23ab499e06b5cb5e35b8403b298f8c
권채린 <lin1041840@gmail.com>
2025-08-26 03:31:56 +0900

    [feat] security 수정

--- project/src/main/java/ict/project/oauth/oauth2client/config/SecurityConfig.java
+++ project/src/main/java/ict/project/oauth/oauth2client/config/SecurityConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import ict.project.oauth.oauth2client.service.CustomOAuth2UserService;
@@ -27,6 +29,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/access-guest").permitAll()
                         .requestMatchers("/questions/**").permitAll()
+                        .requestMatchers("/api/users/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Login(oauth2 -> oauth2
@@ -41,5 +44,10 @@ public class SecurityConfig {
                 );
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
