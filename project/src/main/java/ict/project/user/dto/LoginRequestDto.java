334924af683b94ecc5c05bc2f168e4ed9bbbf898
권채린 <lin1041840@gmail.com>
2025-08-26 03:30:48 +0900

    [feat] Login dto

---
+++ project/src/main/java/ict/project/user/dto/LoginRequestDto.java
@@ -0,0 +1,13 @@
+package ict.project.user.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class LoginRequestDto {
+    private String email;
+    private String password;
+}
