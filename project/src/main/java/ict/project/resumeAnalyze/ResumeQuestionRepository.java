95b4e51b5a839dc38da2f51f427cbfbff9d79301
권채린 <lin1041840@gmail.com>
2025-08-29 00:36:02 +0900

    이력서 질문 레포 생성

---
+++ project/src/main/java/ict/project/resumeAnalyze/ResumeQuestionRepository.java
@@ -0,0 +1,6 @@
+package ict.project.resumeAnalyze;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResumeQuestionRepository extends JpaRepository<ResumeQuestion, Integer> {
+}
